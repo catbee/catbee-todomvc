23baa5c46e9f86b1ef7153f8f972e5f8629723fc
a.khimtsov <a.khimtsov@2gis.ru>
2015-12-02 15:37:05 +0600

    jsdoc

--- actions/utilsAction.js
+++ actions/utilsAction.js
@@ -1,4 +1,9 @@
 module.exports = {
+  /**
+   * Sets input's text in state
+   * @param {string} path, value - path to value in state, value - input's value
+   * @param {object} state
+   */
   setInputValue ({ path, value }, state) {
     let currentPath = path && path.split('.');
     state.set(currentPath, value);
