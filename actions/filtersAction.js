bafda78e9d4eb64805acc2ade96135276acce0ed
a.khimtsov <a.khimtsov@2gis.ru>
2015-12-02 14:45:59 +0600

    adding jsdoc

--- actions/filtersAction.js
+++ actions/filtersAction.js
@@ -5,11 +5,21 @@ const FILTERS_LIST = [
 ];
 
 module.exports = {
+  /**
+   * Sets application's filters
+   * @param {object} args
+   * @param {object} state
+   */
   setFiltersList (args, state) {
     state.set(['filters', 'list'], FILTERS_LIST);
   },
 
-  setActiveFilter ( { index }, state = {} ) {
+  /**
+   * Sets active filter
+   * @param {number} index - index of active filter
+   * @param {object} state
+   */
+  setActiveFilter ( { index }, state ) {
     let filterIndex = index || 0;
     let filters = state.get(['filters', 'list']);
 
--- actions/statementsAction.js
+++ actions/statementsAction.js
@@ -1,8 +1,18 @@
 module.exports = {
+  /**
+   * Sets application's title
+   * @param {object} args
+   * @param {object} state
+   */
   setTitle (args, state) {
     state.set(['statements', 'title'], 'TodoMVC');
   },
 
+  /**
+   * Sets application's header
+   * @param {object} args
+   * @param {object} state
+   */
   setHeader (args, state) {
     state.set(['statements', 'header'], 'todos');
   }
