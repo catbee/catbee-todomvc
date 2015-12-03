4549d0baead0d39e8b31e2c998a841774e2c9f19
a.khimtsov <a.khimtsov@2gis.ru>
2015-12-03 09:30:32 +0600

    history service

--- browser.js
+++ browser.js
@@ -2,10 +2,13 @@ var catbee = require('catbee');
 var handlebars = require('catberry-handlebars');
 var storage = require('./services/storage');
 var helpers = require('./services/helpers');
+var history = require('./services/history');
 
 var cat = catbee.create({});
 handlebars.register(cat.locator);
 helpers.register(cat.locator);
 storage.register(cat.locator);
+history.register(cat.locator);
+
 
 cat.startWhenReady();
