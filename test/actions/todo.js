713ad361e080a5af47c1e9bdddaea906caea32dc
a.khimtsov <a.khimtsov@2gis.ru>
2015-12-04 09:37:57 +0600

    - add key "-l" to lab command - add tests for loadStorageTodos method of todoAction.js - add tests for setTodos method of todoAction.js - add tests for validateNewTodo method of todoAction.js - add tests for addNewTodo method of todoAction.js - add tests for removeTodo method of todoAction.js

---
+++ test/actions/todo.js
@@ -0,0 +1,176 @@
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var assert = require('assert');
+var todoAction = require('../../actions/todoAction');
+
+var Baobab = require('baobab');
+var storage = require('../../services/storage');
+
+var ServiceLocator = require('catberry-locator');
+var catLocator = new ServiceLocator();
+
+catLocator.registerInstance('config', {});
+storage.register(catLocator);
+
+lab.experiment('todoAction.loadStorageTodos method', () => {
+  let state,
+    output;
+
+  lab.beforeEach(done => {
+    state = new Baobab({});
+    output = {};
+    global.localStorage = {};
+    done();
+  });
+
+  lab.test('Successful service answer', done => {
+    global.localStorage = {
+      getItem: () => {
+        return JSON.stringify({id: 'test'});
+      }
+    };
+    output = {
+      success: todos => {
+        assert.deepEqual(todos, {todos: {id: 'test'}});
+        done();
+      }
+    };
+
+    todoAction.loadStorageTodos({}, state, output, { locator: catLocator });
+  });
+
+  lab.test('Unsuccessful service answer', done => {
+    global.localStorage = {
+      getItem: () => {
+        return {id: 'test'};
+      }
+    };
+    output = {
+      error: ({error}) => {
+        assert.equal(error instanceof Error, true);
+        done();
+      }
+    };
+    todoAction.loadStorageTodos({}, state, output, { locator: catLocator });
+  });
+});
+
+lab.experiment('todoAction.setTodos method', () => {
+  let state;
+
+  lab.beforeEach(done => {
+    state = new Baobab({});
+    done();
+  });
+
+  lab.test('Set empty todos', done => {
+    let todos = '';
+    todoAction.setTodos({todos}, state);
+    assert.deepEqual(state.get(['todos']), []);
+    done();
+  });
+
+  lab.test('Set few todos', done => {
+    let todos = ['todo1', 'todo2'];
+    todoAction.setTodos({todos}, state);
+    assert.deepEqual(state.get(['todos']), todos);
+    done();
+  });
+});
+
+lab.experiment('todoAction.validateNewTodo method', () => {
+  let state,
+    output,
+    value;
+
+  lab.beforeEach(done => {
+    state = new Baobab({});
+    output = {};
+    done();
+  });
+
+  lab.test('Successful validating', done => {
+    value = [1, 2, 3];
+    output = {
+      success: () => {
+        done();
+      }
+    };
+
+    todoAction.validateNewTodo({value}, state, output);
+  });
+
+  lab.test('Unsuccessful validating', done => {
+    value = [];
+    output = {
+      error: () => {
+        done();
+      }
+    };
+
+    todoAction.validateNewTodo({value}, state, output);
+  });
+});
+
+lab.experiment('todoAction.addNewTodo method', () => {
+  let state;
+
+  lab.beforeEach(done => {
+    state = new Baobab({});
+    done();
+  });
+
+  lab.test('Adding one todo, asserts all properties', done => {
+    let inputValue = 'test input value';
+    state.set(['form', 'input'], inputValue);
+    state.set(['todos'], []);
+
+    todoAction.addNewTodo({}, state);
+    assert.equal(state.get(['todos']).length, 1);
+    assert.equal(state.get(['todos', 0, 'status']), 'active');
+    assert.strictEqual(state.get(['todos', 0, 'shown']), true);
+    assert.equal(state.get(['todos', 0, 'name']), inputValue);
+    done();
+  });
+
+  lab.test('Adding three todos, asserts length', done => {
+    let inputValue = 'test input value';
+    state.set(['form', 'input'], inputValue);
+    state.set(['todos'], []);
+
+    todoAction.addNewTodo({}, state);
+    todoAction.addNewTodo({}, state);
+    todoAction.addNewTodo({}, state);
+    assert.equal(state.get(['todos']).length, 3);
+    done();
+  });
+});
+
+lab.experiment('todoAction.removeTodo method', () => {
+  let state,
+    id;
+
+  lab.beforeEach(done => {
+    state = new Baobab({});
+    done();
+  });
+
+  lab.test('Removes todo when only one todo in todos', done => {
+    id = 1;
+    state.set(['todos'], [ {id} ]);
+
+    todoAction.removeTodo({id}, state);
+    assert.equal(state.get(['todos']).length, 0);
+    done();
+  });
+
+  lab.test('Removes todo when three todo in todos', done => {
+    id = 1;
+    state.set(['todos'], [ {id}, {id: 2}, {id: 3} ]);
+
+    todoAction.removeTodo({id}, state);
+    assert.equal(state.get(['todos']).length, 2);
+    assert.strictEqual(!state.get(['todos', {id}]), true, 'Should be removed');
+    done();
+  });
+});
